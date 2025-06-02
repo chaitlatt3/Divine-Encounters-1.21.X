cfedc265200113702dcdc331242dfc6fd5b39bba
chaitlatt3 <cpmuragundi@gmail.com>
2025-06-02 16:44:13 -0400

    Added new item: Ambrosia with temp texture

--- src/main/java/chai/divinen/datagen/ModItemTagProvider.java
+++ src/main/java/chai/divinen/datagen/ModItemTagProvider.java
@@ -1,12 +1,5 @@
 package chai.divinen.datagen;
 
-import chai.divinen.item.ModItems;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.registry.RegistryWrapper;
-
-import java.util.concurrent.CompletableFuture;
-
 public class ModItemTagProvider {
 
 }
